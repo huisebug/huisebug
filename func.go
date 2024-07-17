4a419164faaa8c3276f5860ee8a3e9af09bc3997
wuyunfei <wuyunfei@rcrai.com>
2024-07-17 16:00:33 +0800

    update

--- func.go
+++ func.go
@@ -1,8 +1,8 @@
 package huisebug
 
-// 目的是返回一个闭包函数，用来检查传入的字符串是否等于 v。这种方法通常用于创建定制的字符串比较函数
-// Fixed value
-// Transmitting values
+// 目的是返回一个闭包函数，用来检查传入的字符串是否等于Fixedvalue。这种方法通常用于创建定制的字符串比较函数
+// Fixedvalue:固定值
+// Transmittingvalue:传递值
 // StringCompare("wyf")("wyf1")
 func StringCompare(f string) func(string) bool {
 	return func(t string) bool {
