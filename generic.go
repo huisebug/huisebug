e236a826ec24df67a4bef31c3d559f908b084cc4
wuyunfei <wuyunfei@rcrai.com>
2024-07-19 14:32:52 +0800

    ComparableSet

--- generic.go
+++ generic.go
@@ -145,6 +145,8 @@ func (NS NamerSlice[T]) IndexByName(name string) int {
 	return -1
 }
 
+// 使用 IndexByName 方法查找具有指定名称的元素的索引。如果没有找到，该方法返回一个负数。
+// 返回指针：如果找到，则返回指向该元素的指针。
 func (NS NamerSlice[T]) Find(name string) *T {
 	i := NS.IndexByName(name)
 	if i < 0 {
@@ -154,32 +156,67 @@ func (NS NamerSlice[T]) Find(name string) *T {
 	return &NS[i]
 }
 
+// 调用 item.Name() 获取 item 的名称。这个方法假设 T 类型有一个 Name 方法。
+// 查找索引：使用 NS.IndexByName 方法查找该名称在 NamerSlice 切片中的索引。
 func (NS NamerSlice[T]) IndexOf(item T) int {
 	return NS.IndexByName(item.Name())
+}
+
+// 返回一个布尔值，表示 NS[i] 是否应该排在 NS[j] 之前。具体来说，Less 方法返回 true 表示 NS[i] 排在 NS[j] 之前，false 表示 NS[j] 排在 NS[i] 之前。
+// 使用 strings.Compare 函数比较两个名称的字典序。strings.Compare(a, b) 的返回值是：
+// 小于 0 如果 a 小于 b，
+// 等于 0 如果 a 等于 b，
+// 大于 0 如果 a 大于 b。
+func (NS NamerSlice[T]) Less(i, j int) bool {
+	return strings.Compare(NS[i].Name(), NS[j].Name()) < 0
 }
 
 func (NS NamerSlice[T]) Len() int {
 	return len(NS)
 }
 
-func (NS NamerSlice[T]) Less(i, j int) bool {
-	return strings.Compare(NS[i].Name(), NS[j].Name()) < 0
-}
-
-// Swap swaps the elements with indexes i and j.
+// NamerSlice 类型实现了 sort.Interface 接口的三个方法：Len、Less 和 Swap。
+// Less 方法定义了排序的比较规则（按名称的字典序升序排序）。
+// 通过 sort.Sort 函数对 people 切片进行排序，最终输出按名称升序排序的人员列表。
 func (NS NamerSlice[T]) Swap(i, j int) {
 	NS[i], NS[j] = NS[j], NS[i]
 }
 
-type GSet[K comparable] map[K]struct{}
+// map 的键是 T 类型，值是 struct{}。使用空结构体作为值可以节省内存，因为空结构体不占用额外空间，仅用于表示键的存在性
+// 一个类型为可比较类型的集合
+type ComparableSet[T comparable] map[T]struct{}
 
-func (gm GSet[K]) Add(ks ...K) {
-	for _, key := range ks {
-		gm[key] = struct{}{}
+// NewComparableSet 创建一个新的 ComparableSet 实例
+func NewComparableSet[T comparable]() ComparableSet[T] {
+	return make(ComparableSet[T])
+}
+
+// Add 向集合中添加元素
+func (CS ComparableSet[T]) Add(cs ...T) {
+	for _, key := range cs {
+		CS[key] = struct{}{}
 	}
 }
 
-func (g1 GSet[K]) Sub(g2 GSet[K]) {
+// ComparableSlice 转换为 一个新的 ComparableSet 实例
+func ComparableSliceConvertComparableSet[T comparable](v []T) ComparableSet[T] {
+	s := ComparableSet[T]{}
+	s.Add(v...)
+	return s
+}
+
+// Remove 从集合中删除元素
+func (CS ComparableSet[T]) Remove(item T) {
+	delete(CS, item)
+}
+
+// Contains 检查集合中是否存在元素
+func (CS ComparableSet[T]) Contains(item T) bool {
+	_, exists := CS[item]
+	return exists
+}
+
+func (g1 ComparableSet[T]) Sub(g2 ComparableSet[T]) {
 	for key := range g1 {
 		if _, has := g2[key]; has {
 			delete(g1, key)
@@ -187,23 +224,17 @@ func (g1 GSet[K]) Sub(g2 GSet[K]) {
 	}
 }
 
-func NewGSet[K comparable](v []K) GSet[K] {
-	s := GSet[K]{}
-	s.Add(v...)
-	return s
-}
-
-func (gm GSet[K]) Values() []K {
-	keys := []K{}
-	for key := range gm {
+func (CS ComparableSet[T]) Values() []T {
+	keys := []T{}
+	for key := range CS {
 		keys = append(keys, key)
 	}
 
 	return keys
 }
 
-func Keys[M ~map[K]V, K comparable, V any](m M) []K {
-	r := make([]K, 0, len(m))
+func Keys[M ~map[T]V, T comparable, V any](m M) []T {
+	r := make([]T, 0, len(m))
 	for k := range m {
 		r = append(r, k)
 	}
